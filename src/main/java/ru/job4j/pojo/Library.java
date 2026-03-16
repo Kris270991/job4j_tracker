f8a545efd3aeaa77fa205c2898861c9cdf99e7c4
kristinalazareva <kristinkaell@mail.ru>
2026-03-16 17:39:56 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,41 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book bookOne = new Book("Мои машинки", 63);
+        Book bookTwo = new Book("Красная шапочка", 7);
+        Book bookThree = new Book("Clean code", 150);
+        Book bookFour = new Book("Котенок Шмяк: уютные истории", 96);
+        Book[] books = new Book[4];
+        books[0] = bookOne;
+        books[1] = bookTwo;
+        books[2] = bookThree;
+        books[3] = bookFour;
+
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " - " + book.getCountOfPages());
+        }
+
+        System.out.println("Change the position of the books");
+        Book newbook = books[0];
+        books[0] = books[3];
+        books[3] = newbook;
+
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " - " + book.getCountOfPages());
+        }
+
+        System.out.println("Shows only Clean Code");
+
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            if (book.getName().equals("Clean code")) {
+                System.out.println(book.getName() + " - " + book.getCountOfPages());
+            }
+        }
+
+    }
+}
