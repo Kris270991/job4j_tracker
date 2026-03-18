93c6eda8671c90a4990fb06dd69e00f8683e6383
kristinalazareva <kristinkaell@mail.ru>
2026-03-18 17:13:27 +0300

    2. Модель данных. [#174683 #560275]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -6,6 +6,6 @@ public class College {
         student.setFullName("Лазарева Кристина Александровна");
         student.setDateOfEntrance(2025, 9, 1);
         student.setGroupNumber(131);
-        System.out.println("ФИО: " + student.getFullName() + ".\nгруппа: " + student.getGroupNumber() + ".\nдата поступления: " + student.getDateOfEntrance() + '.');
+        System.out.println("ФИО: " + student.getFullName() + '.' + System.lineSeparator() + "группа: " + student.getGroupNumber() + '.' + System.lineSeparator() + "дата поступления: " + student.getDateOfEntrance() + '.');
     }
 }
