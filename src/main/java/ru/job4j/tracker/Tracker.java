4a3e3624fa0bd75858b21da88176f7ec84a98559
kristinalazareva <kristinkaell@mail.ru>
2026-03-20 01:56:46 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -11,15 +13,44 @@ public class Tracker {
         return item;
     }
 
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        int resultSize = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item != null) {
+                result[resultSize] = item;
+                resultSize++;
+            }
+        }
+        result = Arrays.copyOf(result, resultSize);
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int resultSize = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item != null && key.equals(item.getName())) {
+                result[resultSize] = item;
+                resultSize++;
+            }
+        }
+        result = Arrays.copyOf(result, resultSize);
+        return result;
+
+    }
+
     public Item findById(int id) {
-        Item rsl = null;
+        Item result = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getId() == id) {
-                rsl = item;
+                result = item;
                 break;
             }
         }
-        return rsl;
+        return result;
     }
 }
