bb4d74cb8c36f98f6fa46cf9fcab101764c5af3b
kristinalazareva <kristinkaell@mail.ru>
2026-04-03 14:43:35 +0300

    не залитое в уроки

--- src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -2,8 +2,8 @@ package ru.job4j.inheritance;
 
 public class ReportUsage {
     public static void main(String[] args) {
-        JSONReport report = new JSONReport();
-        String text = report.generate("name", "body");
+        HtmlReport report = new HtmlReport();
+        String text = report.generate("Report's name", "Report's body");
         System.out.println(text);
     }
 }
