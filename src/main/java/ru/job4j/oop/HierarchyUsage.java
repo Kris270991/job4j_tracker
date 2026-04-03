4bad8abb6ef03573a126a86eb6cee88f6b3a655c
kristinalazareva <kristinkaell@mail.ru>
2026-04-03 14:59:26 +0300

    не залитое в уроки

--- src/main/java/ru/job4j/oop/HierarchyUsage.java
+++ src/main/java/ru/job4j/oop/HierarchyUsage.java
@@ -3,13 +3,13 @@ package ru.job4j.oop;
 public class HierarchyUsage {
     public static void main(String[] args) {
         /* создаем объекта класса Car. */
-        Car car = new Car();
+        Car car = new Car("b", "m");
         /* делаем приведение к типу родителя Transport. */
         Transport transport = car;
         /* делаем приведение к типу родителя Object. */
         Object object = car;
         /* Приведение типа при создании объекта. */
-        Object objectCar = new Car();
+        Object objectCar = new Car("b", "m");
         /* Приведение типа за счет понижения по иерархии. */
         Car carFromObject = (Car) objectCar;
 
