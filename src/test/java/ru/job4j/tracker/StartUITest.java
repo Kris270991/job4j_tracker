4e26cde2a7c4e628f67b6337566d15cafda3887b
kristinalazareva <kristinkaell@mail.ru>
2026-04-11 00:47:40 +0300

    12.1 Зависимость от System.out [#33568]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,31 +8,47 @@ class StartUITest {
 
     @Test
     void whenCreateItem() {
+        Output output = new StubOutput();
         Input input = new MockInput(new String[]{"0", "Item name", "1"});
         Tracker tracker = new Tracker();
-        UserAction[] actions = {new CreateAction(), new ExitAction()};
-        new StartUI().init(input, tracker, actions);
+        UserAction[] actions = {new CreateAction(output), new ExitAction(output)};
+        new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
     }
 
     @Test
     void whenReplaceItem() {
+        Output output = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Replaced item"));
         String replasedName = "New item name";
         Input input = new MockInput(new String[]{"0", "1", "New item name", "1"});
-        UserAction[] actions = {new ReplaceAction(), new ExitAction()};
-        new StartUI().init(input, tracker, actions);
+        UserAction[] actions = {new ReplaceAction(output), new ExitAction(output)};
+        new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replasedName);
     }
 
     @Test
     void whenDeleteItem() {
+        Output output = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("deleted item"));
         Input input = new MockInput(new String[]{"0", "1", "1"});
-        UserAction[] actions = {new DeleteAction(), new ExitAction()};
-        new StartUI().init(input, tracker, actions);
+        UserAction[] actions = {new DeleteAction(output), new ExitAction(output)};
+        new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findById(item.getId())).isNull();
+    }
+
+    @Test
+    void whenExit() {
+        Output output = new StubOutput();
+        Input input = new MockInput(new String[]{"0"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {new ExitAction(output)};
+        new StartUI(output).init(input, tracker, actions);
+        assertThat(output.toString()).isEqualTo("Меню:" + System.lineSeparator()
+                + "0. Завершить программу" + System.lineSeparator()
+                + "=== Завершение программы ===" + System.lineSeparator());
+
     }
 }
