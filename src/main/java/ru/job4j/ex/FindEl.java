76b0355cf518a9a17b1b6914aaac49d7f0a43058
kristinalazareva <kristinkaell@mail.ru>
2026-04-17 16:26:16 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws UserInputException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key != null && key.equals(value[i])) {
+                result = i;
+            }
+        }
+        if (result == -1) {
+            throw new UserInputException("ElementNotFoundException");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] value = new String[]{"one", "two", "three"};
+        String key = "three";
+        try {
+            System.out.println(indexOf(value, key));
+        } catch (UserInputException e) {
+            e.printStackTrace();
+        }
+    }
+}
